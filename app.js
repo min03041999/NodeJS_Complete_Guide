31ae07539c23b49cdb10fc3332ac2ac2f311630c
Min Kookie <min03041999@gmail.com>
2022-08-24 11:41:18 +0700

    Sharing data across requests and users

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
 // Adding a 404 Error Page
--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,13 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../utils/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   // res.send("<h1>Hello from Express!</h1>");
+  console.log("shop.js", adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
