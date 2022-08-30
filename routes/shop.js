1154e290474fb4927bb01dcc56aa2bc2324fc04e
Min Kookie <min03041999@gmail.com>
2022-08-30 11:19:41 +0700

    commit ejs

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,14 @@ router.get("/", (req, res, next) => {
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
   // console.log("shop.js", adminData.products);
   const products = adminData.products;
-  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
