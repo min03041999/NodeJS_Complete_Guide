10d30a233141eadf1805e81ea70aaf4f9c3a43bc
Min Kookie <min03041999@gmail.com>
2022-08-31 08:21:23 +0700

    finish controller

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const errorController = require("./controllers/error");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -15,14 +17,11 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 // Adding a 404 Error Page
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 // Create Server
 app.listen(3000);
