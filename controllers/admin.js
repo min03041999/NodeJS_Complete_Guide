dec37723baa35f510d92a2f5e22eca09e5677c9a
Min Kookie <min03041999@gmail.com>
2022-09-13 09:30:06 +0700

    editing the product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    editing: false,
   });
 };
 
@@ -12,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, price, description);
+  const product = new Product(null, title, imageUrl, price, description);
   product.save();
   res.redirect("/");
 };
@@ -21,13 +22,36 @@ exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
     return res.redirect("/");
-  } else {
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
     res.render("admin/edit-product", {
       pageTitle: "Edit Product",
       path: "/admin/edit-product",
       editing: editMode,
+      product: product,
     });
-  }
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updateTitle = req.body.title;
+  const updateImageUrl = req.body.imageUrl;
+  const updatePrice = req.body.price;
+  const updateDesc = req.body.description;
+  const updateProduct = new Product(
+    prodId,
+    updateTitle,
+    updateImageUrl,
+    updatePrice,
+    updateDesc
+  );
+  updateProduct.save();
+  res.redirect("/admin/products");
 };
 
 exports.getProducts = (req, res, next) => {
